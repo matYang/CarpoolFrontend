938e0b9f7d2150f556f0ead84d896cbd7df4a237
matYang <uwse@me.com>
2013-11-01 11:37:10 -0400

    removing all caching from Notification manager, totally state less except for timeStamp

--- scripts/dataManagers/notificationManager.js
+++ scripts/dataManagers/notificationManager.js
@@ -8,54 +8,15 @@
 		this.sessionManager = sessionManager;
 		this.userManager = userManager;
 	
-		this.notification = new Notification();
-		this.notifications = new Notifications();
 		this.timeStamp = new Date();
 
 		this.sessionManager.resgisterManager(this);
-	};
-
-
-	NotificationManager.prototype.getNotifications = function() {
-		return this.notifications;
 	};
 
 	NotificationManager.prototype.release = function() {
 		this.timeStamp = new Date();
 	};
 
-
-	NotificationManager.prototype.fetchNotification = function(userId, callback){
-		if (typeof userId !== 'number'){
-			Constants.dWarn("NotificationManager::fetchNotification:: userId invalid");
-			return;
-		}
-		if (!this.sessionManager.hasSession()){
-			Constants.dWarn("NotificationManager::fetchNotification:: session does not exist, exit");
-			return;
-		}
-
-		var self = this;
-		//passing reset true to make sure notifications are always sync with server
-		this.notifications.fetch({
-			reset: true,
-			data: $.param({ 'userId': userId}),
-			dataType:'json',
-			success:function(model, response){
-				self.timeStamp = new Date();
-				if(callback){
-					callback.success(self.notifications);
-				}
-			},
-			error: function(model, response){
-				Constants.dWarn("NotificationManager::fetchNotification:: fetch failed with response:");
-				Constants.dLog(response);
-				if(callback){
-					callback.error();
-				}
-			}
-		});
-	};
 
 	NotificationManager.prototype.checkNotification = function(notificationId, callback) {
 		if (typeof notificationId !== 'number'){
@@ -68,16 +29,17 @@
 		}
 
 		var self = this;
-
-		this.notification.overrideUrl(this.apis.notification_notification);
-		this.notification.set('notificationId', notificationId);
-		this.notification.save({},{
+	
+		var notification = new Notification();
+		notification.overrideUrl(this.apis.notification_notification);
+		notification.set('notificationId', notificationId);
+		notification.save({},{
 			data: $.param({'userId': this.sessionManager.getUserId() }),
 			dataType:'json',
 			success:function(model, response){
 				self.timeStamp = new Date();
 				if(callback){
-					callback.success(self.notification);
+					callback.success(notification);
 				}
 			},
 			error: function(model, response){
@@ -101,16 +63,16 @@
 		}
 
 		var self = this;
-
-		this.notification.overrideUrl(this.apis.notification_notification);
-		this.notification.set('notificationId', notificationId);
-		this.notification.destroy({
+		var notification = new Notification();
+		notification.overrideUrl(this.apis.notification_notification);
+		notification.set('notificationId', notificationId);
+		notification.destroy({
 			data: $.param({ 'userId': this.sessionManager.getUserId()}),
 			dataType:'json',
 			success:function(model, response){
 				self.timeStamp = new Date();
 				if(callback){
-					callback.success(self.notification);
+					callback.success(notification);
 				}
 			},
 			error: function(model, response){
