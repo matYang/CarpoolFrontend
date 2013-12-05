0aecc49edde08e6ddd810e3aeb28951b95a50708
matYang <uwse@me.com>
2013-12-05 10:03:08 -0500

    create tpl_letter

---
+++ templates/tpl_letter.js
@@ -0,0 +1,27 @@
+<script type="text/template" id="tpl_letter">
+
+  <div id = 'letter_left_panel'>
+      <div id = 'letter_user_search'>
+          <input id = 'search_contact_input' />
+      </div>    
+      <div id = 'letter_system_list'>
+          <div class="letterContactListEntry" id="contactList_-1"><img src="res/personal/default-avatar.jpg"><span>系统</span></div>
+  
+      </div>
+      <div id = 'letter_user_list'></div>
+  </div>
+  <div id = 'letter_main_panel'>
+      <div id = 'letter_targetUser_info'>
+          <img id='letter_toUser_pic' src='' />
+          <div id='letter_toUser_name'></div>
+      </div>
+      <div id = 'letter_message_panel'></div>
+      <div id = 'letter_send_panel'>
+          <textarea id='letter_input'></textarea>
+          <div id='letter_send_button' class='button'>发送</div>
+          <div id='letter_flash'></div>
+      </div>
+  </div>
+
+
+</script>
