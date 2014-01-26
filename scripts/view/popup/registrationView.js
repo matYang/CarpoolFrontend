18a0a39ad36b054eb96f511728aef0c1e4a01656
Fangcun Yu <godomoe@gmail.com>
2014-01-26 02:15:21 -0500

    register validators and stuff

--- scripts/view/popup/registrationView.js
+++ scripts/view/popup/registrationView.js
@@ -18,13 +18,23 @@ var RegistrationView = Backbone.View.extend({
         this.render(1);
     },
     getLatLng: function (loc) {
-        var request = {}, me = this;
+        var request = {}, that = this;
         loc.set("defaultId", -1);
         request.address = loc.get("pointName") + "," + loc.get("city") + "," + loc.get("province");
         var result = this.geocoder.geocode(request, function (geocodeResults, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 loc.set("lat", geocodeResults[0].geometry.location.lat());
                 loc.set("lng", geocodeResults[0].geometry.location.lng());
+                that.valid.location = false;
+                if (!that.registerInfo.pivot) {
+                    $("#pivotLocation").parent().addClass("wrong").append("<p id='vlocation' title='请选择一个地区'></p>");
+                    return;
+                }
+                if (!that.registerInfo.pivot.isInRange(that.registerInfo.location)) {
+                    $("#pivotLocation").parent().addClass("wrong").append("<p id='vlocation' title='对不起，该地区暂时不在服务区'></p>");
+                    return;
+                }
+                that.valid.location = true;
             } else {
                 Info.warn('Geocode was not successful for the following reason: ' + status);
             }
@@ -41,6 +51,9 @@ var RegistrationView = Backbone.View.extend({
             this.$password = $("#registerPasswordInput");
             this.$confirm = $("#registerPasswordConfirmInput");
             this.$email = $('#registerEmailInput');
+            this.$year = $("#birthyear");
+            this.$month = $("#birthmonth");
+            this.$day = $("#birthday");
             this.bindEvents();
             this.bindValidator();
         } else {
@@ -56,78 +69,158 @@ var RegistrationView = Backbone.View.extend({
     },
     bindValidator: function(){
         this.valid = {};
+        //email regex
         var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             that = this;
         this.$email.on("blur", function (e) {
             $("#vemail").remove();
+            that.$email.parent().removeClass("wrong");
             if (re.test(that.$email.val())) {
                 that.valid.email = true;
+                that.registerInfo.email = that.$email.val();
                 that.$email.after('<span id="vemail" class="right"></span>');
             } else {
                 that.valid.email = false;
-                that.$email.after('<span id="vemail" class=""></span>');
+                that.$email.parent().addClass("wrong").append('<p id="vemail" title="邮箱地址有误"></p>');
             }
 
         });
         this.$password.add(this.$confirm).on("blur", function (e) {
-            debugger;
             var p = that.$password.val();
             $("#vpass").remove();
             $("#vpass2").remove();
-            if (p && p.length >= 6 && that.$password.val() === that.$confirm.val()) {
+            that.$password.parent().removeClass("wrong");
+            that.$confirm.parent().removeClass("wrong");
+            if (p && p.length >= 8 && that.$password.val() === that.$confirm.val()) {
                 that.$password.after('<span id="vpass" class="right"></span>');
                 that.$confirm.after('<span id="vpass2" class="right"></span>');
                 that.valid.password = true;
+                that.registerInfo.password = that.$password.val();
             } else {
-                that.$password.after('<span id="vpass" class="wrong"></span>');
-                that.$confirm.after('<span id="vpass2" class="wrong"></span>');
+                if (!p || p.length < 8) {
+                    that.$password.parent().addClass("wrong").append('<p id="vpass" title="密码长度至少为8位"></p>');
+                } else {
+                    that.$confirm.parent().addClass("wrong").append('<p id="vpass2" title="两次输入密码不匹配"></p>');
+                }
                 that.valid.password = false;
             }
         });
-
+        this.$year.add(this.$month).add(this.$day).on("blur", function (e) {
+            var y = that.$year.val(), m = that.$month.val(), d = that.$day.val(), bdvalid = true;
+            debugger;
+            if ( y && m && d ) {
+                $(this).parent().removeClass("wrong");
+                $("#vbirth").remove();
+                y = Utilities.toInt(y);
+                m = Utilities.toInt(m);
+                d = Utilities.toInt(d);
+                if ( y < 1910 || y > 2012 ) {
+                    bdvalid = false;
+                }
+                if ( m < 1 || m > 12 ) {
+                    bdvalid = false;
+                }
+                if (m < 1 || m > 31) {
+                    bdvalid = false;
+                } else if (m === 4 || m === 6 ||m === 9 || m === 11){
+                    bdvalid = bdvalid && (d <= 30);
+                } else if (m === 2) {
+                    if ( y % 4 === 0 ) {
+                        bdvalid = bdvalid && (d <= 29);
+                    } else {
+                        bdvalid = bdvalid && (d <= 28)
+                    }
+                }
+                if (!bdvalid) {
+                    $(this).parent().addClass("wrong").append("<p id='vbirth' title='请填写正确的日期'></p>");
+                    that.registerInfo.birthday = null;
+                    that.valid.birthday = false;
+                } else {
+                    $(this).parent().append("<span id='vbirth' class='right'></span>");
+                    that.registerInfo.birthday = new Date();
+                    that.registerInfo.birthday.setFullYear(y);
+                    that.registerInfo.birthday.setMonth(m);
+                    that.registerInfo.birthday.setDate(d);
+                    that.valid.birthday = true;
+                }
+            } else {
+                that.registerInfo.birthday = null;
+            }
+        });
     },
     bindEvents: function(){
-        var self = this;
+        var that = this;
         $("#registerGenderSelect").on("click", function (e) {
             $(this).find(".radio_box_checked").removeClass("radio_box_checked");
             var g = $(e.target).addClass("radio_box_checked").attr("data-id");
             if (g === "male") {
-                self.registerInfo.gender = Constants.gender.female;
+                that.registerInfo.gender = Constants.gender.female;
             } else if (g === "male") {
-                self.registerInfo.gender = Constants.gender.male;
+                that.registerInfo.gender = Constants.gender.male;
             }
         });
 
         $('#pivotLocation').on('click', function (e) {
-            self.closeLocationDropDown();
-            self.locationDropDownView = new LocationDropDownView($('#pivotLocation'), self);
+            that.closeLocationDropDown();
+            that.locationDropDownView = new LocationDropDownView($('#pivotLocation'), that);
             e.stopPropagation();
         });
+
         $("#myAddress").on('blur', function (e) {
-            self.getLatLng(self.registerInfo.location);
+            if ($(this).val() && $(this).val() !== "请输入具体地址") {
+                that.registerInfo.location.set("pointAddress", $(this).val())
+            }
+            that.getLatLng(that.registerInfo.location);
+        }).on('focus', function() {
+            if ($(this).val() === "请输入具体地址") {
+                $(this).val("");
+            }
         });
         $("#complete").on("click", function(){
-            if (!self.registerInfo.pivot) {
+            if (that.valid.email) {
+            } else {
+                if ($("#vemail").length === 0) {
+                    that.$email.parent().addClass("wrong").append('<p id="vemail" title="请输入你的邮箱"></p>');
+                }
                 return;
             }
-            if (!self.registerInfo.pivot.isInRange(self.registerInfo.location)) {
-                //TODO: notify user
+            if (that.valid.password) {
+            } else {
+                if ($("#vpass").length === 0) {
+                    that.$password.parent().addClass("wrong").append('<p id="vpass" title="请输入一个密码"></p>');
+                    that.$confirm.parent().addClass("wrong").append('<p id="vpass2" title="请输入一个密码"></p>');
+                }
+                return;   
+            }
+            if (that.valid.location) {
+            } else {
+                if ($("#vlocation").length === 0) {
+                    $('#pivotLocation').parent().addClass("wrong").append('<p id="vlocation" title="请选择你的地址"></p>');
+                }
                 return;
             }
-            self.registerInfo.email = $('#registerEmailInput').val();
-            self.registerInfo.password = $('#registerPasswordInput').val();
+            // if (that.valid.birthday ) {
+
+            // } else {
+            //     return;
+            // }
+            that.registerInfo.email = $('#registerEmailInput').val();
+            that.registerInfo.password = $('#registerPasswordInput').val();
 
             var user = new User();
-            user.set('location', self.registerInfo.location);
-            user.set('gender', self.registerInfo.gender);
-            user.set('email', self.registerInfo.email);
-            user.set('password', self.registerInfo.password);
-            user.set()
+            user.set('location', that.registerInfo.location);
+            user.set('gender', that.registerInfo.gender);
+            user.set('email', that.registerInfo.email);
+            user.set('password', that.registerInfo.password);
+
+            if (that.registerInfo.birthday) {
+                user.set("birthday", that.registerInfo.birthday);
+            }
             app.userManager.registerUser(user, {
                 success: function(){
-                    self.emailCache = user.get('email');
+                    that.emailCache = user.get('email');
                     app.navigate("register/finish");
-                    //self.render(4);
+                    //that.render(4);
                     Info.alert('注册成功');
                 },
 
