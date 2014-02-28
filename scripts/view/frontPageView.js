9ef1ade0a59fad83703d028937d23f8586234d34
Fangcun Yu <godomoe@gmail.com>
2014-02-28 01:00:04 -0500

    bug fix

--- scripts/view/frontPageView.js
+++ scripts/view/frontPageView.js
@@ -17,7 +17,6 @@ var FrontPageView = Backbone.View.extend({
         this.departLocation = new UserLocation();
         this.arrivalLocation = new UserLocation();
         this.locationDirection = Constants.LocationDirection.from;
-        this.render();
         this.messages = app.storage.getRecentMessages();
         if (this.messages) {
             this.renderRecents(this.messages);
